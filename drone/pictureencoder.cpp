a8755c456fae7818046091c5465228610e9b470a
Clement Guellec <you@example.com>
2023-05-06 19:59:17 +0200

    /add decode

--- drone/pictureencoder.cpp
+++ drone/pictureencoder.cpp
@@ -3,6 +3,7 @@
 #include <bitset>
 #include <iostream>
 #include <QByteArray>
+#include <QtCore/QBitArray>
 PictureEncoder::PictureEncoder(QString s_image_name,QString s_data)
 {
     this->s_image_name = s_image_name;
@@ -14,9 +15,7 @@ PictureEncoder::PictureEncoder(QString s_image_name,QString s_data)
 
 
 void PictureEncoder::encode_data()
-{
-
-}
+{}
 
 void PictureEncoder::encode_picture()
 {
@@ -51,126 +50,30 @@ void PictureEncoder::encode_picture()
 
 QString PictureEncoder::decode_picture()
 {
-
-    QByteArray messageData;
-    const unsigned char *pixels = this->i_image.bits();
+unsigned char *pixels = this->i_image.bits();
     int imageSize = this->i_image.width() * this->i_image.height();
 
-    for (int i = 0; i < 7*8; i++) {
-        unsigned char lastBit = pixels[i] & 0x01;
-        messageData.append(lastBit);
+    QBitArray messageBits(16);  
+    for (int i = 0; i < messageBits.size(); i++) {
+        unsigned char pixel = pixels[i];
+        bool bit = (pixel & 1) != 0;
+        messageBits.setBit(i,bit);
     }
 
-    QString message = QString::fromUtf8(messageData);
-    printf("aa\n%s\n",message.toStdString());
+    QByteArray messageData;
+    int numBits = messageBits.size();
+    for (int i = 0; i < numBits/8; i += 1) {
+        char byte = 0;
+        for (int j = 0; j < 8; j++) {
+            byte |= (messageBits.testBit(i*8+j) << 7-j);
+            
+        }
+        messageData.append(byte);
+    }
+    QString message = QString(messageData.constData());
     return message;
-
-    
-    // const unsigned char *pixels = this->i_image.bits();
-    // int imageSize = this->i_image.width() * this->i_image.height();
-
-    // // Récupérer la taille du message caché
-    // int messageSize = 0;
-    // for (int i = 0; i < 8; i++) {
-    //     int bit = pixels[i] & 1;
-    //     messageSize |= (bit << i);
-    // }
-
-    // // Vérifier que la taille du message est cohérente avec l'image
-    // if (messageSize + 8 > imageSize) {
-    //     printf("erorsize");
-    //     //qDebug() << "Le message n'a pas été trouvé dans l'image.";
-    //     return "";
-    // }
-
-    // // Récupérer le message caché
-    // QByteArray messageData;
-    // for (int i = 0; i < messageSize; i++) {
-    //     unsigned char byte = 0;
-    //     for (int j = 0; j < 8; j++) {
-    //         int bit = pixels[(i+1)*8+j] & 1;
-    //         byte |= (bit << j);
-    //     }
-    //     messageData.append(byte);
-    // }
-
-    // // Vérifier le préfixe du message
-    // QString message = QString::fromUtf8(messageData);
-    // if (!message.startsWith("S")) {
-    //     printf("eroS");
-    //     //qDebug() << "Le message n'a pas été trouvé dans l'image.";
-    //     return message;
-    // }
-
-    // // Extraire le message de la chaîne
-    // int messageLength = message.mid(1, 8).toInt();
-    // message = message.mid(9, messageLength);
-
-    // return message;
 }
-    // int imageSize = this->i_image.width() * this->i_image.height();
-    // QString message = this->s_data;
-
-    // int messageSize = this->b_data_tab.size();
-    // unsigned char *messageBytes = (unsigned char *)this->b_data_tab.constData();
-
-    // if (messageSize > imageSize) {
-    //     printf("Le message est trop grand pour être caché dans l'image.");
-    //     return;
-    // }
-
-
-//     int i = 0;
-
-    // for (int y = 0; y < this->i_image.height(); y++) {
-    //    for (int x = 0; x < this->i_image.width(); x++) {
-    //        QRgb pixel = this->i_image.pixel(x, y);
-    //        int gray = qGray(pixel);
-
-    //        this->i_image.setPixel(x, y, gray);
-    //    }
-    //    }
-           
-//         //    if (this->b_data_tab.length() > 0) {
-//         //         i++;
-//         //        gray = (gray & ~1) | (this->b_data_tab.at(0));
-//         //        //if(gray%2 == 0 and )
-               
-//         //        this->b_data_tab.remove(0, 1);
-//         //        printf("\ndata restance : %d",this->b_data_tab.length());
-//         //    }
-//         //    
-//        }
-//    }
-//     this->i_image.save("../../test2_DroneIMG37337.png");
-//     printf("\n nb rep : %d",i);
-
-    // // encode number
-    // for(int i = 0; i < this->b_data_tab.length(); i++)
-    // {
-    //     pixels[(i-1)*8+k]
-    // }
-    // if()
-    // int i = 0;
-
-
-    // while (i < messageSize) {
-    //     if (messageBytes[i] >= '0' && messageBytes[i] <= '9') {
-    //         // Cacher le nombre de caractères du message
-    //         int length = messageBytes[i] - '0';
-    //         i++;
-    //         for (int j = 0; j < length && i < messageSize; j++) {
-    //             // Cacher chaque caractère du message
-    //             for (int k = 0; k < 8 && i < messageSize; k++) {
-    //                 int bit = (messageBytes[i] >> k) & 1;
-    //                 pixels[(i-1)*8+k] = (pixels[(i-1)*8+k] & 0xFE) | bit;
-    //             }
-    //             i++;
-    //         }
-    //     } else {
-    //         i++;
-    //     }
-    // }
+    
 
 void PictureEncoder::make_sendable()
 {
